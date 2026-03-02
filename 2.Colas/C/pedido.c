719cd58e569b8f8b6f3eff01dc7403a2d7fafa09
Jose Carlos Rodríguez Cañas <pepepfoter15@gmail.com>
2026-03-02 19:44:18 +0100

    Añadiendo codigo respecto a las 3.Pilas

--- 2.Colas/C/pedido.c
+++ 2.Colas/C/pedido.c
@@ -1,4 +1,11 @@
+//
 // pedido.c
+// EstructurasC
+//
+// Creado por José Carlos Rodríguez Cañas
+// Copyright (c) 2026 José Carlos Rodríguez Cañas
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "pedido.h"
